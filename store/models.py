786ec3c0ca405a4259dce13e266dc24d7db58761
Fazel <fazelsamar@gmail.com>
2022-09-16 16:25:06 +0430

    Change cart model

--- store/models.py
+++ store/models.py
@@ -107,7 +107,7 @@ class CartItem(models.Model):
     quantity = models.PositiveSmallIntegerField()
 
     class Meta:
-        unique_together = [['product', 'quantity']]
+        unique_together = [['cart', 'product']]
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
