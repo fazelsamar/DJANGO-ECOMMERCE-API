82d81dbf5d90356ac9f6ba30cd9b5273000e2426
Fazel <fazelsamar@gmail.com>
2022-09-17 22:44:14 +0430

    Add delete method in cart endpoint

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.db.models.aggregates import Count
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -53,8 +53,9 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
 
 
-class CartViewSet(CreateModelMixin, 
-                    RetrieveModelMixin, 
-                    GenericViewSet):
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
     queryset = models.Cart.objects.prefetch_related('items__product')
     serializer_class = serializers.CartSerializer
