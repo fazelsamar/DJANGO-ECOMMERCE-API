5afae18ccf6c5912114d28c523c0f58817d37f65
Fazel <fazelsamar@gmail.com>
2022-09-18 18:09:56 +0430

    Add djoser configs

--- eccomerce_api/settings.py
+++ eccomerce_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+    'djoser',
     'debug_toolbar',
 
     'playgroud',
@@ -151,3 +152,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
