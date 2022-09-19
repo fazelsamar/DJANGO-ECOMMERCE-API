6f7cf8203b81203d4a7535fc86751d74bde798bd
Fazel <fazelsamar@gmail.com>
2022-09-19 18:57:41 +0430

    Update jwt settings

--- eccomerce_api/settings.py
+++ eccomerce_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,6 +162,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
