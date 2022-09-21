93361d8ebaa87d50814259f2d4b9f306b2cf8b1c
Fazel <fazelsamar@gmail.com>
2022-09-21 23:35:56 +0430

    Fix orderitems related model

--- store/models.py
+++ store/models.py
@@ -87,8 +87,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='orderitems')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
